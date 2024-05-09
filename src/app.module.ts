0572e32daf2d33def2f985bc4cd26500f5790cda
Minji Kim <sml07113@gmail.com>
2024-05-10 02:52:30 +0900

    feat: TypeORM 관련 라이브러리 설치 및 DB 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeORMConfig from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeORMConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
